209c03052fa6065cb36e879ed6154c411cdaee30
Valliappan Raman <rvalliappan@hydropoint.com>
2025-08-14 10:45:30 +0530

    final set of changes

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -10,8 +10,6 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
-import { error } from 'console';
-import { stat } from 'fs';
 
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
